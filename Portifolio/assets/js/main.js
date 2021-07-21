50559c4e6d7d0d188b0886bb43c7dd52eca4679b
Thiago <thiago.ralves02@gmail.com>
2021-07-20 23:00:51 -0300

    dark mode, responsive

--- Portifolio/assets/js/main.js
+++ Portifolio/assets/js/main.js
@@ -2,6 +2,7 @@ let inputNome = document.querySelector('#nome')
 let inputEmail = document.querySelector('#email')
 let textareaMensagem = document.querySelector('#mensagem')
 let btnEnviar = document.querySelector('#enviar')
+let checkbox = document.querySelector('#fs')
 
 inputNome.addEventListener('keyup',() =>{
     if(inputNome.value.length < 2){
@@ -30,3 +31,22 @@ textareaMensagem.addEventListener('keyup',() =>{
 btnEnviar.addEventListener('click',() =>{
     alert('Formulario enviado com sucesso!')
 })
+
+checkbox.addEventListener('change',()=>{
+    if (checkbox.checked) {
+        document.querySelector('body').style.backgroundColor = '#192734'
+        document.querySelector('#habilidades').style.backgroundColor = '#192734'
+        document.querySelector('body').style.color = '#ddd'
+        document.querySelector('p').style.color = '#ddd'
+        document.querySelector('span').style.color = '#ddd'
+    }
+    else{
+        document.querySelector('body').style.backgroundColor = '#fff'
+        document.querySelector('#habilidades').style.backgroundColor = '#d7e7ff'
+        document.querySelector('body').style.color = 'black'
+        document.querySelector('p').style.color = '#3359ec'
+        document.querySelector('span').style.color = '#3359ec'
+    }
+    
+})
+
